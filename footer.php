51d0ea74df113804a58a502928b9bcf3434d76bc
codewhy <i@iobiji.com>
2019-12-29 23:54:59 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,25 +1,29 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php
-if(!empty($this->options->qiniu)){
-  $qurl = str_replace($this->options->siteUrl,$this->options->qiniu.'/',$this->options->themeUrl);
-}else{
+if (!empty($this->options->qiniu)) {
+  $qurl = str_replace($this->options->siteUrl, $this->options->qiniu . '/', $this->options->themeUrl);
+} else {
   $qurl = $this->options->themeUrl;
 }
-$appminjs = $qurl.'/assets/app/js/app.min.js';
-$lazyload = $qurl.'/assets/app/js/lazyload.js';
-$casper = $qurl.'/assets/app/js/casper.js';
-$comments = $qurl.'/assets/app/js/comments.js';
+$appminjs = $qurl . '/assets/app/js/app.min.js';
+$lazyload = $qurl . '/assets/app/js/lazyload.js';
+$casper = $qurl . '/assets/app/js/casper.js';
+$comments = $qurl . '/assets/app/js/comments.js';
 ?>
 
 <footer class="site-footer">
   <div class="container d-flex justify-content-sm-between justify-content-center text-center">
     <div class="copyright">
-      <p><?php if(!null==$this->options->beian):?><a href="http://www.miitbeian.gov.cn" target="_blank" rel="nofollow noopener"><?php echo $this->options->beian ?></a> | <?php endif ?>Powered by TYPECHO. Copyright &copy; 2019. Crafted with <a href="https://github.com/JaydenForYou/CREAMy/" target="_blank" rel="noopener nofollow">CREAMy</a>.</p>
+      <p><?php if (!null == $this->options->beian): ?><a href="http://www.miitbeian.gov.cn" target="_blank"
+                                                         rel="nofollow noopener"><?php echo $this->options->beian ?></a> | <?php endif ?>
+        Powered by TYPECHO. Copyright &copy; 2019. Crafted with <a href="https://github.com/JaydenForYou/CREAMy/"
+                                                                   target="_blank" rel="noopener nofollow">CREAMy</a>.
+      </p>
     </div>
     <nav class="social-links d-none d-sm-block">
       <ul>
-        <li class="social-link"><a href="<?php echo $this->options->weibo?>"><i class="fab fa-weibo"></i></a></li>
-        <li class="social-link"><a href="<?php echo $this->options->twitter?>"><i class="fab fa-twitter"></i></a></li>
+        <li class="social-link"><a href="<?php echo $this->options->weibo ?>"><i class="fab fa-weibo"></i></a></li>
+        <li class="social-link"><a href="<?php echo $this->options->twitter ?>"><i class="fab fa-twitter"></i></a></li>
       </ul>
     </nav>
   </div>
@@ -49,15 +53,27 @@ $comments = $qurl.'/assets/app/js/comments.js';
 </div>
 <div id="return-to-top" class="fixed-tools animated"><i class="fas fa-angle-double-up"></i></div>
 </div>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
 <script>
+  //ajax加载文章
+  /*  var current_page = 1;
+
+    function load_more_post() {
+      current_page++;
+      var load_post_url = window.location.href + "index.php/page/" + current_page + "/?load_type=ajax";
+      $.get(load_post_url, function (html) {
+        console.log('加载：' + load_post_url);
+        $('#blockGroup').append($(html));
+      })
+    }*/
+
   var APPID = '<?php $this->options->APPID()?>';
   var APPKEY = '<?php $this->options->APPKEY()?>';
   var tongji = '<?php $this->options->tongji()?>';
 </script>
-<script type="text/javascript" src="//cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
-<script src='<?=$casper?>'></script>
-<?php if(Utils::isEnabled('enablePJAX','JConfig')): ?>
-  <?php if(Utils::isEnabled('enableLazyload','JConfig')): ?>
+<script src='<?= $casper ?>'></script>
+<?php if (Utils::isEnabled('enablePJAX', 'JConfig')): ?>
+  <?php if (Utils::isEnabled('enableLazyload', 'JConfig')): ?>
     <script>
       var isLZ = true;
     </script>
@@ -69,23 +85,23 @@ $comments = $qurl.'/assets/app/js/comments.js';
   <script src="//cdn.bootcss.com/jquery.pjax/2.0.1/jquery.pjax.js"></script>
   <script src='//unpkg.com/nprogress@0.2.0/nprogress.js'></script>
   <script>
-  var iSPJAX = true;
+    var iSPJAX = true;
   </script>
-  <?php else:?>
+<?php else: ?>
   <script>
-  var iSPJAX = false;
+    var iSPJAX = false;
   </script>
 <?php endif ?>
-<script src='<?=$comments?>'></script>
+<script src='<?= $comments ?>'></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script type="text/javascript" src="<?=$appminjs?>"></script>
-<?php if(Utils::isEnabled('enableLazyload','JConfig')&&$this->is('index')||$this->is('search')): ?>
-  <script src="<?=$lazyload?>"></script>
+<script type="text/javascript" src="<?= $appminjs ?>"></script>
+<?php if (Utils::isEnabled('enableLazyload', 'JConfig') && $this->is('index') || $this->is('search')): ?>
+  <script src="<?= $lazyload ?>"></script>
   <script type="text/javascript">
     $("div.lazyload").lazyload();
   </script>
-<?php elseif(Utils::isEnabled('enableLazyload','JConfig')&&$this->is('page')||$this->is('post')): ?>
-  <script src="<?=$lazyload?>"></script>
+<?php elseif (Utils::isEnabled('enableLazyload', 'JConfig') && $this->is('page') || $this->is('post')): ?>
+  <script src="<?= $lazyload ?>"></script>
   <script type="text/javascript">
     $("img.lazyload").lazyload();
   </script>
