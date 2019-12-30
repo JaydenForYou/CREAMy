ecdd7d4077ec844dd994fd123e001302bbebbb6d
codewhy <i@iobiji.com>
2019-12-31 00:10:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,64 +9,7 @@
  */
 
 if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<?php if (isset($_GET['load_type']) and $_GET['load_type'] == 'ajax'): ?>
-  <?php while ($this->next()): ?>
-    <?php if ($this->sequence % 2 == 0): ?>
-      <article class="post-card align-right">
-    <?php else: ?>
-      <article class="post-card align-left">
-    <?php endif ?>
-    <a href="<?php $this->permalink() ?>" class="post-card-image-link">
-      <?php if (Utils::isEnabled('enableLazyload', 'JConfig')): ?>
-        <div class="post-card-image lazyload" data-src="<?php
-        if ($this->fields->thumbnail) {
-          echo $this->fields->thumbnail;
-        } else {
-          echo Utils::getThumbnail();
-        }
-        ?>" style="background-image: url(<?php echo Utils::getThumbnail(); ?>)"></div>
-      <?php else: ?>
-        <div class="post-card-image" style="background-image: url(<?php
-        if ($this->fields->thumbnail) {
-          echo $this->fields->thumbnail;
-        } else {
-          echo Utils::getThumbnail();
-        }
-        ?>)"></div>
-      <?php endif ?>
-    </a>
-    <div class="post-card-content">
-      <header>
-        <ul class="post-tags">
-          <li class="post-tag"><?php $this->category('/'); ?></li>
-        </ul>
-        <h3 class="post-card-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h3>
-      </header>
-      <section class="post-card-excerpt">
-        <p>
-          <?php
-          if ($this->fields->previewContent)
-            $this->fields->previewContent();
-          else
-            $this->excerpt(55, '...');
-          ?>
-        </p>
-      </section>
-      <footer class="post-meta">
-        <ul class="author-list">
-          <li class="author-list-item">
-            <a href="<?php $this->author->permalink(); ?>" class="static-avatar">
-              <?php echo $this->author->gravatar(320, 'G', NULL, 'author-profile-image') ?>
-              <span class="author-profile-name"><?php $this->author(); ?></span>
-            </a>
-          </li>
-        </ul>
-      </footer>
-    </div>
-    </article>
-  <?php endwhile; ?>
-  <?php return; ?>
-<?php endif ?>
+
 <?php
 $this->need('header.php');
 /*$hiddens = '';
@@ -175,9 +118,13 @@ if ($this->_currentPage == ceil($this->getTotal() / $this->parameter->pageSize))
               </a>
             </li>-->
             <div id="next">
-              <li class="page-item"><?php if (ceil($this->getTotal() / $this->parameter->pageSize) > 1): ?><a
+              <li
+                class="page-item"><?php if ($this->_currentPage <> ceil($this->getTotal() / $this->parameter->pageSize)): ?>
+                  <a
                   class="page-link" data-status="0" onclick="ajaxGetMore();return false;"
-                  href="/page/<?php echo $next ?>"">加载更多</a><?php else: ?><span id="bottom" data-status="1">到底啦~</span><?php endif ?></li>
+                  href="/page/<?php echo $next ?>"">加载更多</a><?php else: ?><span id="bottom"
+                                                                                data-status="1">到底啦~</span><?php endif ?>
+              </li>
             </div>
             <!--            <li class="page-item"><a class="page-link">第<? /*= $cpage */ ?>
                 页，共<?php /*echo ceil($this->getTotal() / $this->parameter->pageSize); */ ?>页</a></li>-->
