5235a8fea440a8b39bc46b129f7136e9b8ff8a77
codewhy <i@iobiji.com>
2019-12-30 17:37:07 +0800

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,136 +1,130 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<style>
-  [data-theme=dark] .post-comments .v .vwrap {
-    border:1px solid #080808
-  }
-  [data-theme=dark] .post-comments .v .vwrap .vheader .vinput {
-    border-bottom:.0625rem dashed #b7b7b7
-  }
-  [data-theme=dark] .post-comments .v .vbtn {
-    border:1px solid #080808;
-    background:#252525;
-    color:#b7b7b7
-  }
-  [data-theme=dark] .post-comments .v .vlist .vcard .vhead .vsys {
-    background:#080808;
-    color:#b7b7b7
-  }
-  [data-theme=dark] .post-comments .v .vlist .vcard .vh {
-    border-bottom:.0625rem dashed #080808
-  }
-  [data-theme=dark] .post-comments .v .vlist .vquote {
-    border-left:#080808
-  }
-  [data-theme=dark] .post-comments .v .vcontrol .col {
-    color:#252525
-  }
-  [data-theme=dark] .post-comments .v * {
-    color:#b7b7b7
-  }
-</style>
 <?php
 function threadedComments($comments, $options)
 {
   $commentClass = '';
   if ($comments->authorId) {
     if ($comments->authorId == $comments->ownerId) {
-      $commentClass .= ' comment-by-author';  //如果是文章作者的评论添加 .comment-by-author 样式
+      $commentClass .= ' comment-by-author';
     } else {
-      $commentClass .= ' comment-by-user';  //如果是评论作者的添加 .comment-by-user 样式
+      $commentClass .= ' comment-by-user';
     }
   }
-  $commentLevelClass = $comments->_levels > 0 ? ' comment-child' : ' comment-parent';  //评论层数大于0为子级，否则是父级
+
+  $commentLevelClass = $comments->levels > 0 ? ' comment-child' : ' comment-parent';
+  $depth = $comments->levels + 1;
+
+  if ($comments->url) {
+    $author = '<a href="' . $comments->url . '"target="_blank"' . ' rel="external nofollow">' . $comments->author . '</a>';
+  } else {
+    $author = $comments->author;
+  }
   ?>
-  <li id="<?php $comments->theId(); ?>" class="comment-body<?php
-  if ($comments->levels > 0) {
-    echo ' comment-child';
+
+  <li id="li-<?php $comments->theId(); ?>" class="comment comment-body<?php
+  if ($depth > 1 && $depth < 3) {
+    echo ' children ';
+    $comments->levelsAlt('comment-level-odd', ' comment-level-even');
+  } else if ($depth > 2) {
+    echo ' comment-child2';
     $comments->levelsAlt(' comment-level-odd', ' comment-level-even');
   } else {
     echo ' comment-parent';
   }
+
   $comments->alt(' comment-odd', ' comment-even');
-  echo $commentClass;
   ?>">
-    <div class="vcard" id="<?php $comments->theId(); ?>">
-      <img class="vimg" src="<?php echo Utils::gGravatar($comments->author); ?>">
-      <div class="vh" id="<?php $comments->theId(); ?>">
-        <div class="vhead">
-          <a class="vnick" rel="nofollow" href="<?php echo $comments->url; ?>"
-             target="_blank"><?php echo $comments->author; ?></a><?php echo Comments::parseUserAgent($comments->agent); ?>
+    <div id="<?php $comments->theId(); ?>">
+      <?php
+      $host = 'https://secure.gravatar.com';
+      $url = '/avatar/';
+      $size = '80';
+      $default = 'mm';
+      $rating = Helper::options()->commentsAvatarRating;
+      $hash = md5(strtolower($comments->mail));
+      $avatar = $host . $url . $hash . '?s=' . $size . '&r=' . $rating . '&d=' . $default;
+      ?>
+      <div class="comment-block" onclick="">
+        <div class="comment-info u-flex">
+          <div class="comment-avatar u-flex0">
+            <img class="avatar" src="<?php echo $avatar ?>" width="<?php echo $size ?>" height="<?php echo $size ?>"/>
+          </div>
+          <div class="comment-meta u-flex1 u-flexColumn">
+            <div class="comment-author" itemprop="author">
+              <a href="" rel="external nofollow" class="url"><?php echo $author; ?></a>
+              <span class="comment-reply-link u-cursorPointer"><?php $comments->reply('回复'); ?></span>
+            </div>
+            <div class="comment-time" itemprop="datePublished"
+                 datetime="<?php $comments->date(); ?>"><?php $comments->date('M j, Y'); ?></div>
+          </div>
         </div>
-        <div class="vmeta">
-          <span class="vtime"><?php $comments->date('Y-m-d H:i'); ?></span>
-          <span class="vat"><button onclick="onClick('<?php $comments->theId(); ?>','<?php echo $comments->author; ?>')"
-                                    class="vsubmit vbtn">回复</button></span>
-        </div>
-        <div class="vcontent">
-          <p>
-            <?php $comments->content(); ?>
-          </p>
+        <div class="comment-content" itemprop="description">
+          <?php echo getCommentAt($comments->coid) ?><?php $comments->content(); ?>
         </div>
       </div>
     </div>
     <?php if ($comments->children) { ?>
-      <div class="vquote">
+      <div class="comment">
         <?php $comments->threadedComments($options); ?>
       </div>
     <?php } ?>
   </li>
 <?php } ?>
-<div class="post-comments">
-  <?php if($this->user->hasLogin()): ?>
-    <p>
-      <?php _e('当前登录身份: '); ?>
-      <a href="<?php $this->options->profileUrl(); ?>">
-        <?php $this->user->screenName(); ?>
-      </a>.
-      <a href="<?php $this->options->logoutUrl(); ?>" title="Logout" no-pjax>
-        <?php _e('退出'); ?>&raquo;</a>
-    </p>
-  <?php endif ?>
-  <div id="<?php $this->respondId(); ?>" class="v">
-    <form method="post" action="comment" id="comment-form" role="form">
-      <div class="vwrap">
-        <div class="vheader item3">
-          <input name="author" id="author" placeholder="昵称" value="<?php $this->remember('author'); ?>" class="vnick vinput" type="text" required><input name="mail"
-                                                                                                             id="mail"
-                                                                                                             placeholder="邮箱"
-                                                                                                             class="vmail vinput"
-                                                                                                             type="email" value="<?php $this->remember('mail'); ?>"><input
-                  name="url" placeholder="网址(http://)" class="vlink vinput" type="text" value="<?php $this->remember('url'); ?>">
-        </div>
-        <div class="vedit">
-          <textarea id="textarea" name="text" id="mail" class="veditor vinput" placeholder="请您理智发言，共建美好社会！"></textarea>
-        </div>
-        <div class="vcontrol">
-          <div class="col col-100 text-right">
-            <button id="reply" type="submit" title="Cmd|Ctrl+Enter" class="vsubmit vbtn">回复</button>
-          </div>
-        </div>
-        <div style="display:none;" class="vmark">
-        </div>
-      </div>
-    </form>
-    <div class="vinfo" style="display:none;">
-      <div class="vcount col">
-      </div>
+
+<div id="comments">
+  <?php $this->comments()->to($comments); ?>
+  <?php if ($comments->have()): ?>
+    <h3 class="responses-title"><?php $this->commentsNum(_t('Comments : %d ')); ?></h3>
+
+    <?php $comments->listComments(); ?>
+
+    <div class="lists-navigator clearfix">
+      <?php $comments->pageNav('←', '→', '2', '...'); ?>
     </div>
-    <?php $this->comments()->to($comments); ?>
-    <?php if ($comments->have()): ?>
-    <div class="vlist">
-        <?php $comments->listComments(); ?>
+
+  <?php endif; ?>
+
+  <?php if ($this->allow('comment')): ?>
+    <div id="<?php $this->respondId(); ?>" class="respond">
+
+      <h3 id="response" class="comments-title"><?php _e('发表留言'); ?></h3>
+
+      <form method="post" action="<?php $this->commentUrl() ?>" id="comment-form" class="responsesForm" role="form">
+        <p class="comment-note">人生在世，错别字在所难免，无需纠正。</p>
+        <?php if ($this->user->hasLogin()): ?>
+          <p><?php _e('登录身份: '); ?><a
+              href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>. <a
+              href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a></p>
+        <?php else: ?>
+          <p class="comment-form-author comment-form-input">
+            <label for="author"><?php _e('称呼'); ?></label>
+            <input type="text" name="author" id="author" class="inputGroup" value="<?php $this->remember('author'); ?>"
+                   required/>
+          </p>
+          <p class="comment-form-email comment-form-input">
+            <label
+              for="mail"<?php if ($this->options->commentsRequireMail): ?><?php endif; ?>><?php _e('Email'); ?></label>
+            <input type="text" name="mail" id="mail" class="inputGroup"
+                   value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?> />
+          </p>
+          <p class="comment-form-url comment-form-input">
+            <label for="url"<?php if ($this->options->commentsRequireURL): ?><?php endif; ?>><?php _e('网站'); ?></label>
+            <input type="text" name="url" id="url" class="inputGroup" placeholder="<?php _e('http://'); ?>"
+                   value="<?php $this->remember('url'); ?>"<?php if ($this->options->commentsRequireURL): ?> required<?php endif; ?> />
+          </p>
+        <?php endif; ?>
+        <p class="comment-form-comment comment-form-input">
+          <label for="textarea"><?php _e('内容'); ?></label>
+          <textarea rows="8" cols="50" name="text" id="comment" class="inputGroup inputTextarea"
+                    required><?php $this->remember('text'); ?></textarea>
+        </p>
+        <p class="form-submit">
+          <button type="submit" id="submit" class="inputSubmit">提交评论</button><?php $comments->cancelReply(); ?>
+        </p>
+
+      </form>
     </div>
-    <?php else: ?>
-    <div class="vempty" style="display:block;">
-      快来做第一个评论的人吧~
-    </div>
-    <?php endif ?>
-    <div class="vpage txt-center">
-    </div>
-    <div class="info">
-      <div class="power txt-right">
-        Powered By <a href="//iobiji.com" target="_blank">Jayden</a><br>
-      </div>
-    </div>
-  </div>
+  <?php else: ?>
+    <h3><?php _e('评论已关闭'); ?></h3>
+  <?php endif; ?>
 </div>
