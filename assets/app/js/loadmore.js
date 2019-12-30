2401b3c3815e12e617b1239df2372fe86227d680
codewhy <admin@iobiji.com>
2019-12-30 18:54:26 +0800

    Add files via upload

---
+++ assets/app/js/loadmore.js
@@ -0,0 +1,35 @@
+function ajaxGetMore() {
+  if ($("#next").html() == "<span>到底啦 ~</span>" || $("#next .page-link").text() == "loading...") {
+    return false;
+  }
+  $("#next .page-link").text("loading...");
+  var url = $("#next a").attr("href");
+  $.ajax({
+    type: "POST",
+    url: url,
+    async: true,
+    success: function (data) {
+      result = $(data).find(".post-card");
+      nextHref = $(data).find(".next").attr("href");
+      $("#blockGroup").append(result.fadeIn(500));
+
+      if (nextHref != undefined) {
+        $("#next a").attr("href", nextHref);
+        $("#next a").text("加载更多");
+      } else {
+        $("#next").html("<span>到底啦 ~</span>");
+      }
+    }
+  });
+  return false;
+}
+
+$(window).scroll(function () {
+  var scrollTop = $(this).scrollTop();
+  var scrollHeight = $(document).height();
+  var windowHeight = $(this).height();
+  if (Math.abs((scrollTop + windowHeight) - scrollHeight) < 1) {
+
+    ajaxGetMore();
+  }
+});
